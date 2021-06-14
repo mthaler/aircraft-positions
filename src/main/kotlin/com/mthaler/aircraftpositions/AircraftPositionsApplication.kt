4ea49e32427501bf0a3d2926e9f0626e178ebc2b
Michael Thaler <michael.thaler@dlr.de>
2021-06-14 19:03:54 +0200

    Added AircraftPositionsApplication, index.html and positions.html.

---
+++ src/main/kotlin/com/mthaler/aircraftpositions/AircraftPositionsApplication.kt
@@ -0,0 +1,11 @@
+package com.mthaler.aircraftpositions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AircraftPositionsApplication
+
+fun main(args: Array<String>) {
+    runApplication<AircraftPositionsApplication>(*args)
+}
