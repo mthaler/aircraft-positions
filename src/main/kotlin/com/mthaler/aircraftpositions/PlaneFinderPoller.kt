1caffadbc3300cb765a72916277c9d093329fe3e
Michael Thaler <michael.thaler@dlr.de>
2021-06-15 07:57:22 +0200

    Updated PlaneFinderPoller.

--- src/main/kotlin/com/mthaler/aircraftpositions/PlaneFinderPoller.kt
+++ src/main/kotlin/com/mthaler/aircraftpositions/PlaneFinderPoller.kt
@@ -5,15 +5,19 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToFlux
+import org.springframework.web.socket.TextMessage
+import java.io.IOException
 
 @EnableScheduling
 @Component
-class PlaneFinderPoller(private val repository: AircraftRepository) {
+class PlaneFinderPoller(private val repository: AircraftRepository, private val handler: WebSocketHandler) {
 
     private val client = WebClient.create("http://localhost:7634/aircraft")
 
     @Scheduled(fixedRate = 1000)
     private fun pollPlanes() {
+        repository.deleteAll()
+
         client.get()
             .retrieve()
             .bodyToFlux<Aircraft>()
@@ -22,5 +26,21 @@ class PlaneFinderPoller(private val repository: AircraftRepository) {
             .forEach { repository.save(it) }
 
         repository.findAll().forEach { println(it) }
+
+        sendPositions()
+    }
+
+    private fun sendPositions() {
+        if (repository.count() > 0) {
+            for (sessionInList in handler.getSessionList()) {
+                try {
+                    sessionInList.sendMessage(
+                        TextMessage(repository.findAll().toString())
+                    )
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
