31c09097619d47ed62b628433554ae7c0e7e6f22
Michael Thaler <michael.thaler@dlr.de>
2021-06-15 12:48:06 +0200

    Updated PositionController.

--- src/main/kotlin/com/mthaler/aircraftpositions/PositionController.kt
+++ src/main/kotlin/com/mthaler/aircraftpositions/PositionController.kt
@@ -3,13 +3,25 @@ package com.mthaler.aircraftpositions
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToFlux
+
 
 @Controller
 class PositionController(private val repository: AircraftRepository) {
 
+    private val client = WebClient.create("http://localhost:7634/aircraft")
+
     @GetMapping("/aircraft")
     fun getCurrentAircraftPositions(model: Model): String {
-        model.addAttribute("currentPositions", repository.findAll())
+        val aircraftFlux = repository.deleteAll()
+            .thenMany(client.get()
+                .retrieve()
+                .bodyToFlux<Aircraft>()
+                .filter { !it.reg.isNullOrEmpty() }
+                .flatMap { repository.save(it) })
+
+        model.addAttribute("currentPositions", aircraftFlux)
         return "positions"
     }
 }
