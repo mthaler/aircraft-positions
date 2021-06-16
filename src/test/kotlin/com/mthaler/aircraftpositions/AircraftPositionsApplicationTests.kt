f60071e5401f8cf1e9e575077384a8b0d228aba5
Michael Thaler <michael.thaler@dlr.de>
2021-06-16 07:34:46 +0200

    Added AircraftPositionsApplicationTests.

---
+++ src/test/kotlin/com/mthaler/aircraftpositions/AircraftPositionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mthaler.aircraftpositions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AircraftPositionsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
